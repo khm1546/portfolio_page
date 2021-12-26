65216a29e995177de069e944b7d11fe33e696318
Hyeongmin Kim <gudalsrla1212@naver.com>
2021-12-27 00:07:30 +0900

    db

--- models.py
+++ models.py
@@ -9,8 +9,6 @@ class Members(Base):
     idx = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(30, 'utf8mb4_unicode_ci'))
 
-    def __init__(self, name, type, question):
+    def __init__(self, name):
         self.name = name
-        self.type = type
-        self.question = question
 
