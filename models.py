e0f7f00204080c8eb8deb53ce27aa7572a5421b0
Hyeongmin Kim <gudalsrla1212@naver.com>
2021-12-27 00:06:51 +0900

    db_update

--- models.py
+++ models.py
@@ -4,12 +4,10 @@ from database import Base
 
 class Members(Base):
 
-    __tablename__ = 'cs'
+    __tablename__ = 'yujin'
 
     idx = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(20, 'utf8mb4_unicode_ci'))
-    type = Column(String(10))
-    question = Column(String(10))
+    name = Column(String(30, 'utf8mb4_unicode_ci'))
 
     def __init__(self, name, type, question):
         self.name = name
